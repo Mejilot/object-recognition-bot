4b8d4fb31ea01b08f972f7f008b11f93c91d1863
Eugene <sharronr@student.21-school.ru>
2023-03-11 01:52:16 +0700

    main fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from bot.main import TBot
 import os
+
+
 def main():
-    new_bot = Bot(os.environ.get("TELEBOTTOKEN"))
+    new_bot = TBot(os.environ.get("TELEBOTTOKEN"))
     new_bot.start_bot()
 
-    BOT.infinity_polling()
+
 if __name__ == "__main__":
     main()
